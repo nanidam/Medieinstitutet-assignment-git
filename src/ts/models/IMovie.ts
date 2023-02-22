64b7fccafb647844eb0d70ae0eb47957eefef28e
NANI <nani7dam@gmail.com>
2023-02-22 20:07:30 +0100

    render movies

--- src/ts/models/IMovie.ts
+++ src/ts/models/IMovie.ts
@@ -5,10 +5,10 @@ export interface IMovie {
   // id: number;
   // original_language: string;
   // original_title: string;
-  overview: string;
+  // overview: string;
   // popularity: number;
   poster_path: string;
-  // release_date: string;
+  release_date: string;
   title: string;
   // video: boolean;
   vote_average: number;
